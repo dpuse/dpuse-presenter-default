4f85b5bdcffe0122481e690f35cfdae1279351f8
Jonathan Terrell <terrell.jm@gmail.com>
2026-07-21 12:42:24 +0200

    v0.1.1043

--- dist/dpuse-presenter-default.es.js
+++ dist/dpuse-presenter-default.es.js
@@ -79,7 +79,7 @@ var e = {
 	status: null,
 	statusId: "alpha",
 	typeId: "presenter",
-	version: "0.1.1042",
+	version: "0.1.1043",
 	usageId: "unknown"
 }, t = {
 	"hr/wrkForce/averageHeadcount": {
